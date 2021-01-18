819da036bce8eda5d436fead3046f4a88650cc8b
Pablo A. Muro Martinez <pablo.muro@gmail.com>
2021-01-18 11:39:32 -0300

    fix initial files

--- routes/api.js
+++ routes/api.js
@@ -3,16 +3,38 @@
 const SudokuSolver = require('../controllers/sudoku-solver.js');
 
 module.exports = function (app) {
-  
-  let solver = new SudokuSolver();
+
+  let sudokuSolver = new SudokuSolver();
+
+  app.route('/api/solve')
+    .post((req, res) => {
+      try {
+        const puzzleString = req.body.puzzle
+        if (!puzzleString) throw new Error('Required field missing')
+
+        sudokuSolver.build(puzzleString)
+        const solution = sudokuSolver.solve()
+        if (!solution) throw new Error('Puzzle cannot be solved')
+
+        res.json({ solution })
+      } catch (error) {
+        return res.json({ error: error.message })
+      }
+
+    });
 
   app.route('/api/check')
     .post((req, res) => {
+      try {
+        const { puzzle, coordinate, value } = req.body
+        if (!puzzle || !coordinate || !value) throw new Error('Required field(s) missing')
 
-    });
-    
-  app.route('/api/solve')
-    .post((req, res) => {
+        sudokuSolver.build(puzzle)
+        const check = sudokuSolver.checkCoordinatePlacement(coordinate, value)
 
+        res.json({ ...check })
+      } catch (error) {
+        return res.json({ error: error.message })
+      }
     });
 };
