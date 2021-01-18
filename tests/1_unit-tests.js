aae6769a7f51a83872b8ab1aeb3da83b494427f4
Pablo A. Muro Martinez <pablo.muro@gmail.com>
2021-01-18 11:39:51 -0300

    implement tests

--- tests/1_unit-tests.js
+++ tests/1_unit-tests.js
@@ -1,9 +1,140 @@
 const chai = require('chai');
 const assert = chai.assert;
 
-const Solver = require('../controllers/sudoku-solver.js');
-let solver;
+const { puzzlesAndSolutions } = require('../controllers/puzzle-strings.js')
+const SudokuSolver = require('../controllers/sudoku-solver.js');
+const sudokuSolver = new SudokuSolver()
 
 suite('UnitTests', () => {
+  suite('Sudoku Unit Tests', () => {
+    test('Logic handles a valid puzzle string of 81 characters', function (done) {
+      let puzzleString = puzzlesAndSolutions[0][0]
+      let errorMessage = null
+      try {
+        sudokuSolver.build(puzzleString)
+      } catch (error) {
+        errorMessage = error.message
+      }
+      assert.isNull(errorMessage)
 
+      done();
+    });
+    test('Logic handles a puzzle string with invalid characters(not 1 - 9 or.)', function (done) {
+      let puzzleString = puzzlesAndSolutions[0][0]
+      let errorMessage = null
+      try {
+        sudokuSolver.build(puzzleString.replace('9', 'A'))
+      } catch (error) {
+        errorMessage = error.message
+      }
+      assert.isNotNull(errorMessage)
+      assert.equal(errorMessage, 'Invalid characters in puzzle')
+
+      done();
+    });
+    test('Logic handles a puzzle string that is not 81 characters in length', function (done) {
+      let puzzleString = '.1.43'
+      let errorMessage = null
+      try {
+        sudokuSolver.build(puzzleString)
+      } catch (error) {
+        errorMessage = error.message
+      }
+      assert.isNotNull(errorMessage)
+      assert.equal(errorMessage, 'Expected puzzle to be 81 characters long')
+
+      done();
+    });
+    test('Logic handles a valid row placement', function (done) {
+      let puzzleString = puzzlesAndSolutions[0][0]
+      sudokuSolver.build(puzzleString)
+      let placement = sudokuSolver.checkRowPlacement(0, 3)
+      assert.isTrue(placement)
+      done();
+    });
+    test('Logic handles an invalid row placement', function (done) {
+      let puzzleString = puzzlesAndSolutions[0][0]
+      sudokuSolver.build(puzzleString)
+      let placement = sudokuSolver.checkRowPlacement(0, 1)
+      assert.isFalse(placement)
+      done();
+    });
+    test('Logic handles a valid column placement', function (done) {
+      let puzzleString = puzzlesAndSolutions[0][0]
+      sudokuSolver.build(puzzleString)
+      let placement = sudokuSolver.checkColPlacement(0, 9)
+      assert.isTrue(placement)
+      done();
+    });
+    test('Logic handles an invalid column placement', function (done) {
+      let puzzleString = puzzlesAndSolutions[0][0]
+      sudokuSolver.build(puzzleString)
+      let placement = sudokuSolver.checkColPlacement(0, 1)
+      assert.isFalse(placement)
+      done();
+    });
+    test('Logic handles a valid region(3x3 grid) placement', function (done) {
+      let puzzleString = puzzlesAndSolutions[0][0]
+      sudokuSolver.build(puzzleString)
+      let placement = sudokuSolver.checkRegionPlacement(0, 0, 9)
+      assert.isTrue(placement)
+      done();
+    });
+    test('Logic handles an invalid region(3x3 grid) placement', function (done) {
+      let puzzleString = puzzlesAndSolutions[0][0]
+      sudokuSolver.build(puzzleString)
+      let placement = sudokuSolver.checkRegionPlacement(0, 0, 1)
+      assert.isFalse(placement)
+      done();
+    });
+    test('Valid puzzle strings pass the solver', function (done) {
+      let puzzleString = puzzlesAndSolutions[0][0]
+      let solutionString = puzzlesAndSolutions[0][1]
+      let errorMessage = null
+      let solution
+      try {
+        sudokuSolver.build(puzzleString)
+        solution = sudokuSolver.solve()
+        if (!solution) throw new Error('Puzzle cannot be solved')
+      } catch (error) {
+        errorMessage = error.message
+      }
+      assert.equal(solution, solutionString)
+      assert.isNull(errorMessage)
+
+      done();
+    });
+    test('Invalid puzzle strings fail the solver', function (done) {
+      let puzzleString = '9.9..5.1.85.4....2432......1...69.83.9.....6.62.71...9......1945....4.37.4.3..6..'
+      let errorMessage = null
+      let solution
+      try {
+        sudokuSolver.build(puzzleString)
+        solution = sudokuSolver.solve()
+        if (!solution) throw new Error('Puzzle cannot be solved')
+      } catch (error) {
+        errorMessage = error.message
+      }
+      assert.isNotNull(errorMessage)
+      assert.equal(errorMessage, 'Puzzle cannot be solved')
+      done();
+    });
+    test('Solver returns the the expected solution for an incomplete puzzzle', function (done) {
+      let puzzleString = puzzlesAndSolutions[0][0]
+      let solutionString = puzzlesAndSolutions[0][1]
+      let errorMessage = null
+      let solution
+      try {
+        sudokuSolver.build(puzzleString)
+        solution = sudokuSolver.solve()
+        if (!solution) throw new Error('Puzzle cannot be solved')
+      } catch (error) {
+        errorMessage = error.message
+      }
+      assert.equal(solution, solutionString)
+      assert.isNull(errorMessage)
+
+      done();
+    });
+  })
 });
--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -6,6 +6,192 @@ const server = require('../server');
 chai.use(chaiHttp);
 
 suite('Functional Tests', () => {
-
+  const validPuzzle = '..9..5.1.85.4....2432......1...69.83.9.....6.62.71...9......1945....4.37.4.3..6..'
+  const validOutput = '769235418851496372432178956174569283395842761628713549283657194516924837947381625'
+  suite('Sudoku Tests Route: /app/solve', () => {
+    const apiRoute = '/api/solve'
+    test('Solve a puzzle with valid puzzle string', function (done) {
+      const puzzle = validPuzzle
+      chai.request(server)
+        .post(apiRoute)
+        .send({ puzzle })
+        .end(function (err, res) {
+          assert.equal(res.status, 200);
+          assert.equal(res.body.solution, validOutput)
+          done();
+        });
+    });
+    test('Solve a puzzle with missing puzzle string', function (done) {
+      chai.request(server)
+        .post(apiRoute)
+        .send({})
+        .end(function (err, res) {
+          assert.equal(res.status, 200);
+          assert.equal(res.body.error, 'Required field missing')
+          done();
+        });
+    });
+    test('Solve a puzzle with invalid characters', function (done) {
+      const puzzle =
+        'ABC..5.1.85.4....2432......1...69.83.9.....6.62.71...9......1945....4.37.4.3..6..';
+      chai.request(server)
+        .post(apiRoute)
+        .send({ puzzle })
+        .end(function (err, res) {
+          assert.equal(res.status, 200);
+          assert.equal(res.body.error, 'Invalid characters in puzzle')
+          done();
+        });
+    });
+    test('Solve a puzzle with incorrect length', function (done) {
+      const puzzle =
+        '..5.1.85.4....2';
+      chai.request(server)
+        .post(apiRoute)
+        .send({ puzzle })
+        .end(function (err, res) {
+          assert.equal(res.status, 200);
+          assert.equal(res.body.error, 'Expected puzzle to be 81 characters long')
+          done();
+        });
+    });
+    test('Solve a puzzle that cannot be solved', function (done) {
+      const puzzle =
+        '9.9..5.1.85.4....2432......1...69.83.9.....6.62.71...9......1945....4.37.4.3..6..';
+      chai.request(server)
+        .post(apiRoute)
+        .send({ puzzle })
+        .end(function (err, res) {
+          assert.equal(res.status, 200);
+          assert.equal(res.body.error, 'Puzzle cannot be solved')
+          done();
+        });
+    });
+  })
+  suite('Sudoku Tests Route: /app/check', () => {
+    const apiRoute = '/api/check'
+    test('Check a puzzle placement with all fields', function (done) {
+      const puzzle = validPuzzle
+      const coordinate = 'A1';
+      const value = '7';
+      chai.request(server)
+        .post(apiRoute)
+        .send({ puzzle, coordinate, value })
+        .end(function (err, res) {
+          assert.equal(res.status, 200);
+          assert.isTrue(res.body.valid);
+          done();
+        });
+    });
+    test('Check a puzzle placement with single placement conflict', function (done) {
+      const puzzle = validPuzzle
+      const coordinate = 'A5';
+      const value = '9';
+      const conflict = ['row'];
+      chai.request(server)
+        .post(apiRoute)
+        .send({ puzzle, coordinate, value })
+        .end(function (err, res) {
+          assert.equal(res.status, 200);
+          assert.isFalse(res.body.valid)
+          assert.deepEqual(res.body.conflict, conflict)
+          done();
+        });
+    });
+    test('Check a puzzle placement with multiple placement conflicts', function (done) {
+      const puzzle = validPuzzle
+      const coordinate = 'A1';
+      const value = '1';
+      const conflict = ['row', 'column'];
+      chai.request(server)
+        .post(apiRoute)
+        .send({ puzzle, coordinate, value })
+        .end(function (err, res) {
+          assert.equal(res.status, 200);
+          assert.isFalse(res.body.valid)
+          assert.deepEqual(res.body.conflict, conflict)
+          done();
+        });
+    });
+    test('Check a puzzle placement with all placement conflicts', function (done) {
+      const puzzle = validPuzzle
+      const coordinate = 'A1';
+      const value = '5';
+      const conflict = ['row', 'column', 'region'];
+      chai.request(server)
+        .post(apiRoute)
+        .send({ puzzle, coordinate, value })
+        .end(function (err, res) {
+          assert.equal(res.status, 200);
+          assert.isFalse(res.body.valid)
+          assert.deepEqual(res.body.conflict, conflict)
+          done();
+        });
+    });
+    test('Check a puzzle placement with missing required fields', function (done) {
+      const puzzle = validPuzzle
+      const value = '1';
+      chai.request(server)
+        .post(apiRoute)
+        .send({ puzzle, value })
+        .end(function (err, res) {
+          assert.equal(res.status, 200);
+          assert.equal(res.body.error, 'Required field(s) missing')
+          done();
+        });
+    });
+    test('Check a puzzle placement with invalid characters', function (done) {
+      const puzzle = validPuzzle.replace('.', 'S')
+      const coordinate = 'A1';
+      const value = '1';
+      chai.request(server)
+        .post(apiRoute)
+        .send({ puzzle, coordinate, value })
+        .end(function (err, res) {
+          assert.equal(res.status, 200);
+          assert.equal(res.body.error, 'Invalid characters in puzzle')
+          done();
+        });
+    });
+    test('Check a puzzle placement with incorrect length', function (done) {
+      const puzzle = '1.4.3'
+      const coordinate = 'A1';
+      const value = '1';
+      chai.request(server)
+        .post(apiRoute)
+        .send({ puzzle, coordinate, value })
+        .end(function (err, res) {
+          assert.equal(res.status, 200);
+          assert.equal(res.body.error, 'Expected puzzle to be 81 characters long')
+          done();
+        });
+    });
+    test('Check a puzzle placement with invalid placement coordinate', function (done) {
+      const puzzle = validPuzzle
+      const coordinate = 'a1';
+      const value = '1';
+      chai.request(server)
+        .post(apiRoute)
+        .send({ puzzle, coordinate, value })
+        .end(function (err, res) {
+          assert.equal(res.status, 200);
+          assert.equal(res.body.error, 'Invalid coordinate')
+          done();
+        });
+    });
+    test('Check a puzzle placement with invalid placement value', function (done) {
+      const puzzle = validPuzzle
+      const coordinate = 'A1';
+      const value = 'X';
+      chai.request(server)
+        .post(apiRoute)
+        .send({ puzzle, coordinate, value })
+        .end(function (err, res) {
+          assert.equal(res.status, 200);
+          assert.equal(res.body.error, 'Invalid value')
+          done();
+        });
+    });
+  })
 });
 
